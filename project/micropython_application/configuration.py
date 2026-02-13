916f6229237c16bd25c4d37fc6b4ddca8d50aee2
e12402368 <e12402368@student.tuwien.ac.at>
2026-02-13 13:45:22 +0100

    micropython basic structure

---
+++ project/micropython_application/configuration.py
@@ -0,0 +1,9 @@
+ssid = "A1-257215"
+key = "AL9AVZ3GKU"
+
+# define labels (order must be the same as in Deepcraft)
+labels = ["nothing", "on", "off", "dimmer","lighter"]
+
+# IMU
+sample_hz = 25
+_sleep_s = 1.0 / sample_hz
---
+++ project/micropython_application/main.py
@@ -0,0 +1,49 @@
+# configuration and parameters
+from configuration import labels
+# Deepcraft
+import array
+import deepcraft_model
+# IMU
+from machine import I2C
+import bmi270
+from configuratioin import sample_hz, _sleep_s
+
+
+# --- set up the deepcraft model ---
+model = deepcraft_model.DEEPCRAFT()
+model.init()
+
+input_dim = model.get_model_input_dim()
+output_dim = model.get_model_output_dim()
+
+# Output-Buffer Array aus Floats, mit 0.0 initialisiert
+output_buffer = array.array(’f’, [0.0] * output_dim)
+
+
+# --- initialize the bmi270 IMU ---
+# documentation: https://www.hackster.io/Infineon_Team/accelerometer-gyroscope-with-psoc-6-and-micropython-757eb3
+i2c = I2C(scl='P0_2', sda='P0_3')
+bmi = bmi270.BMI270(i2c)
+
+
+# --- Main Loop ---
+while True:
+    
+    # --- read sensor data ---
+    # [2,3] array    
+    sensor_data = [bmi.accel(), bmi.gyro()]
+    
+    # --- feed the model with data ---
+    model.enqueue(sensor_data)
+    
+    
+    # --- print result ---
+    # Test if the model has calculated a result
+    if model.dequeue(output_buffer) == 0:
+        # print out the result to the serial port
+        for idx, score in enumerate(output_buffer):
+            # End is a carriage return and not a newline. This way the output gets overwritten
+            print(f"Label: {labels[idx]:<12} Score: {score*100:.2f}%", end='\r')
+            
+            
+            
